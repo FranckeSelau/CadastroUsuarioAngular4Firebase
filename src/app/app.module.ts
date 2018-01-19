b70485ae2b8f889d0bcb00007ba4c6cf48294bfd
francke <francke.selau@gmail.com>
2018-01-19 11:38:54 -0200

    instalando modulo firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { CadastroPessoaComponent } from './cadastro-pessoa/cadastro-pessoa.component';
+
+import { FirebaseConfig } from './../environments/firebase.config';
+import { AngularFireModule } from 'angularfire2/index';
 
 
 @NgModule({
@@ -15,7 +17,10 @@ import { CadastroPessoaComponent } from './cadastro-pessoa/cadastro-pessoa.compo
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(FirebaseConfig)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+export const FirebaseConfig = {
+    apiKey: "AIzaSyBtfAvLqefS9LSOekfPDZcLTrq7mQeA52g",
+    authDomain: "cadastroangular.firebaseapp.com",
+    databaseURL: "https://cadastroangular.firebaseio.com",
+    projectId: "cadastroangular",
+    storageBucket: "cadastroangular.appspot.com",
+    messagingSenderId: "306686761327"
+  };
+  
